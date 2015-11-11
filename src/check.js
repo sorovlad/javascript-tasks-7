3dd248b3a3f5001fc9f0b0d66296ed230f2d4937
sorokinvladimir <fafafa@e1.ru>
2015-11-12 01:02:19 +0500

    sulution

--- src/check.js
+++ src/check.js
@@ -1,5 +1,37 @@
 'use strict';
 
 exports.init = function () {
-
+    Object.prototype.checkHasKeys = function (fields) {
+        return !fields.some(item => {
+            return !this.hasOwnProperty(item);
+        });
+    };
+    Object.prototype.checkHasValueType = function (field, type) {
+        if (!this.hasOwnProperty(field) || typeof this === 'function') {
+            return false;
+        }
+        return typeof this[field] === typeof type();
+    };
+    Object.prototype.checkHasLength = function (count) {
+        if (this.length === count) {
+            return true;
+        }
+        return false;
+    };
+    Object.prototype.checkContainsValues = function (values) {
+        return values.every(item => {
+            return this.hasOwnProperty(item);
+        });
+    };
+    Function.prototype.checkHasParamsCount = function (count) {
+        return this.length === count;
+    };
+    String.prototype.checkHasWordsCount = function (count) {
+        if (typeof this !== 'string') {
+            return false;
+        }
+        return count === this.split(' ').filter(item => {
+            return item.length !== 0;
+        }).length;
+    };
 };
