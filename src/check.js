fd91eecf5f50ee3135b0f0a3ed6671221e8bb7ed
sorokinvladimir <fafafa@e1.ru>
2015-11-23 13:12:45 +0500

    переделал

--- src/check.js
+++ src/check.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports.wrap = function (value) {
+    if (value === undefined) {
+        return {
+            value: undefined
+        };
+    };
     if (value === null) {
         return {
             value: null
@@ -19,98 +24,155 @@ module.exports.wrap = function (value) {
 };
 
 exports.init = function () {
-    var methods = {
+    /*Некоторые методы повторяются из Object в других обьектах,
+    хотя они и так будут присвоены. Как лучше сделать, оставить их
+    только в Object или как есть?*/
+    var objectFns = {
+        hasKeys: function (fields) {
+            return checkHasKeys.bind(this)(fields);
+        },
+        checkContainsKeys: function (keys) {
+            return checkHasKeys.bind(this)(keys);
+        },
+        hasValueType: function (field, type) {
+            return checkHasValueType.bind(this)(field, type);
+        },
+        containsValues: function (values) {
+            return checkContainsValues.bind(this)(values);
+        },
+        hasValues: function (values) {
+            return checkHasValues.bind(this)(values);
+        },
+        isNull: function () {
+            return isNull.bind(this)();
+        }
+    };
+    var functionFns = {
+        hasParamsCount: function (count) {
+            return checkHasParamsCount.bind(this)(count);
+        }
+    };
+    var arrayFns = {
         hasKeys: function (fields) {
             return checkHasKeys.bind(this)(fields);
         },
         hasValueType: function (field, type) {
             return checkHasValueType.bind(this)(field, type);
         },
-        hasLength: function (count) {
-            return checkHasLength.bind(this)(count);
+        checkContainsKeys: function (keys) {
+            return checkHasKeys.bind(this)(keys);
         },
         containsValues: function (values) {
             return checkContainsValues.bind(this)(values);
         },
-        hasParamsCount: function (count) {
-            return checkHasParamsCount.bind(this)(count);
+        hasLength: function (count) {
+            return checkHasLength.bind(this)(count);
+        },
+        hasValues: function (values) {
+            return checkHasValues.bind(this)(values);
+        }
+    };
+    var stringFns = {
+        hasLength: function (count) {
+            return checkHasLength.bind(this)(count);
         },
         hasWordsCount: function (count) {
             return checkHasWordsCount.bind(this)(count);
-        },
-        isNull: function () {
-            return isNull.bind(_this)();
         }
     };
 
-    Object.defineProperty(Object.prototype, 'check', { get: function () {
+    connectToPrototype(Object.prototype, 'check', objectFns);
+    connectToPrototype(Function.prototype, 'check', functionFns);
+    connectToPrototype(Array.prototype, 'check', arrayFns);
+    connectToPrototype(String.prototype, 'check', stringFns);
+
+    function connectToPrototype(objectPrototype, element, methods) {
+        Object.defineProperty(objectPrototype, element, {
+            get: function () {
+                return connect(this, methods);
+            },
+            enumerable: false
+        });
+    };
+
+    function connect(element, methods) {
         var curentMethods = Object.keys(methods).reduce(function (prev, key) {
-            prev[key] = methods[key].bind(this);
+            prev[key] = methods[key].bind(element);
             return prev;
-        }.bind(this), {});
-
-        var _this = this;
-        curentMethods.not = {
-            hasKeys: function (fields) {
-                return !checkHasKeys.bind(_this)(fields);
-            },
-            hasValueType: function (field, type) {
-                return !checkHasValueType.bind(_this)(field, type);
-            },
-            hasLength: function (count) {
-                return !checkHasLength.bind(_this)(count);
-            },
-            containsValues: function (values) {
-                return !checkContainsValues.bind(_this)(values);
-            },
-            hasParamsCount: function (count) {
-                return !checkHasParamsCount.bind(_this)(count);
-            },
-            hasWordsCount: function (count) {
-                return !checkHasWordsCount.bind(_this)(count);
-            },
-            isNull: function () {
-                return !isNull.bind(_this)();
-            }
-        };
+        }.bind(element), {});
+        curentMethods.not = Object.keys(methods).reduce(function (prev, key) {
+            prev[key] = not(methods[key]).bind(element);
+            return prev;
+        }.bind(element), {});
         return curentMethods;
-    }});
-
-    Object.prototype.checkHasKeys = checkHasKeys;
+    }
+    function not(method) {
+        return function () {
+            return !method.apply(this, arguments);
+        };
+    };
+    function checkContainsKeys(keys) {
+        return keys.every(item => {
+            this.hasOwnProperty(item);
+        });
+    };
     function checkHasKeys(fields) {
         return !fields.some(item => {
             return !this.hasOwnProperty(item);
         });
     };
-    Object.prototype.checkHasValueType = checkHasValueType;
     function checkHasValueType(field, type) {
         if (!this.hasOwnProperty(field) || typeof this === 'function') {
             return false;
         }
         return typeof this[field] === typeof type();
     };
-    Object.prototype.checkHasLength = checkHasLength;
     function checkHasLength(count) {
         return this.length === count;
     };
-    Object.prototype.checkContainsValues = checkContainsValues;
     function checkContainsValues(values) {
         return values.every(item => {
             return this.hasOwnProperty(item);
         });
     };
-    Function.prototype.checkHasParamsCount = checkHasParamsCount;
+    function checkHasValues(values) {
+        if (typeof this === 'array') {
+            return this.some(item => {
+                return values.indexOf(item) !== -1;
+            });
+        }
+        for (var key in this) {
+            if (values.indexOf(key) === -1) {
+                return false;
+            }
+        }
+        return true;
+    };
     function checkHasParamsCount(count) {
         return this.length === count;
     };
-    String.prototype.checkHasWordsCount = checkHasWordsCount;
     function checkHasWordsCount(count) {
         return count === this.split(' ').filter(item => {
             return item.length !== 0;
         }).length;
     };
-    Object.prototype.isNull = isNull;
     function isNull() {
         return this.value === null;
     };
+
+    function connectToObjectProto(proto, name, method) {
+        Object.defineProperty(proto, name, {
+            value: method,
+            enumerable: false,
+            writable: true
+        });
+    };
+
+    connectToObjectProto(Object.prototype, 'checkHasKeys', checkHasKeys);
+    connectToObjectProto(Object.prototype, 'checkHasValueType', checkHasValueType);
+    connectToObjectProto(Object.prototype, 'checkHasLength', checkHasLength);
+    connectToObjectProto(Object.prototype, 'checkContainsValues', checkContainsValues);
+    connectToObjectProto(Function.prototype, 'checkHasParamsCount', checkHasParamsCount);
+    connectToObjectProto(String.prototype, 'checkHasWordsCount', checkHasWordsCount);
+    connectToObjectProto(Object.prototype, 'isNull', isNull);
 };
